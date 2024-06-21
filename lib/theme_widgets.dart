479c80bf18bed55317815d9eab45cef31b4f42e7
dorna6 <145118491+dorna6@users.noreply.github.com>
2024-06-21 12:35:50 +0300

    add theme widget

---
+++ lib/theme_widgets.dart
@@ -0,0 +1,73 @@
+//
+// manage all the related app theme widgets,
+// including the themes variabels and there provider.
+//
+
+import 'package:flutter/material.dart';
+
+//
+//
+//
+/////////////////////
+// light theme var //
+/////////////////////
+
+class LightTheme {
+  static final ThemeData theme = ThemeData(
+    brightness: Brightness.light,
+    colorScheme: ColorScheme.light(
+      background: const Color(0xFFf6f6f8), // background color
+      primary: const Color(0xFFffffff), // menu line color
+      secondary: const Color(0xFF000000), // text color
+      shadow: const Color(0xFFe3e3e3), // shadow
+    ),
+  );
+}
+//
+//
+//
+////////////////////
+// dark theme var //
+////////////////////
+
+class DarkTheme {
+  static final ThemeData theme = ThemeData(
+    brightness: Brightness.dark,
+    colorScheme: ColorScheme.dark(
+      background: const Color(0xFF202020),
+      primary: const Color(0xFF2B2B2B),
+      secondary: const Color(0xFFffffff),
+      shadow: const Color(0xFF2B2B2B),
+    ),
+  );
+}
+
+//
+//
+//
+////////////////////
+// theme provider //
+////////////////////
+
+class ThemeProvider extends ChangeNotifier {
+  ThemeData _themeDataStyle = LightTheme.theme;
+
+  ThemeData get themeDataStyle => _themeDataStyle;
+
+  set themeDataStyle(ThemeData themeData) {
+    _themeDataStyle = themeData;
+    notifyListeners();
+  }
+
+  void changeTheme() {
+    if (_themeDataStyle == LightTheme.theme) {
+      themeDataStyle = DarkTheme.theme;
+    } else {
+      themeDataStyle = LightTheme.theme;
+    }
+  }
+
+  bool getCurrentTheme() {
+    return _themeDataStyle == DarkTheme.theme;
+  }
+}
