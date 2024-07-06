ff75f334cf8ff7c65973968ea98e2937200c9c2d
dorna6 <145118491+dorna6@users.noreply.github.com>
2024-07-06 23:15:08 +0300

    work nice, need some improvment in desigen

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/services.dart';
 
 import 'blessing_widget.dart';
 import 'main_text_widget.dart';
+import 'custom_text_widget.dart';
 
 void main() {
   runApp(MyApp());
@@ -30,6 +31,8 @@ class _MyHomePageState extends State<MyHomePage> {
   double rate2 = 0;
   double rate3 = 0;
   double rate4 = 0;
+  double rate5 = 0;
+  double rate6 = 0;
 
   String asset = "";
   double top = 0.0;
@@ -48,7 +51,8 @@ class _MyHomePageState extends State<MyHomePage> {
                 rate2 -= v.scrollDelta! / 1.35;
                 rate3 -= v.scrollDelta! / 1.2;
                 rate4 -= v.scrollDelta! / 1;
-
+                rate5 -= v.scrollDelta! / 2;
+                rate6 -= v.scrollDelta! / 1.15;
               }
             });
           }
@@ -57,56 +61,54 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Stack(
           children: <Widget>[
             Container(
-              height: MediaQuery.of(context).size.height,
+              height: double.infinity,
               color: Color(0xFFe0eff6),
             ),
+            ParallaxWidgetText2(
+                top: rate5,
+                text:
+                    "אַךְ בַּזֶּה אֲנִי חָזָק בְּיוֹתֵר שֶׁאֵלּוּ הָעֲשָׂרָה מִזְמוֹרֵי תְּהִלִּים מוֹעִילִים מְאֹד מְאֹד, וְאָמַר שֶׁהוּא תִּקּוּן הַכְּלָלִי."),
             ParallaxWidget(top: rate0, asset: "assets/img5.png"),
             ParallaxWidget(top: rate1, asset: "assets/img4.png"),
             ParallaxWidget(top: rate2, asset: "assets/img3.png"),
             ParallaxWidget(top: rate3, asset: "assets/img2.png"),
             ParallaxWidget(top: rate4, asset: "assets/img1.png"),
+            ParallaxWidgetText(top: rate6, text: " "),
             ListView(
               children: <Widget>[
                 Container(
-                  height: MediaQuery.of(context).size.height*0.95,
+                  height: MediaQuery.of(context).size.height * 1.0,
                   color: Colors.transparent,
                 ),
-
                 Container(
-                  color: Theme.of(context).colorScheme.background,
-                  width: MediaQuery.of(context).size.width,
-                  padding: EdgeInsets.only(top: 70),
-                  child:
+                    color: Theme.of(context).colorScheme.background,
+                    width: MediaQuery.of(context).size.width,
+                    padding: EdgeInsets.only(top: 20),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        //
+                        // main text
+                        Padding(
+                          padding: const EdgeInsets.only(
+                              left: 25.0, right: 25.0, top: 0.0),
+                          child: mainTextWidget(context: context),
+                        ),
 
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    //
-                    // main text
+                        // Spacer between elements
+                        SizedBox(
+                            height: 15,
+                            width: MediaQuery.of(context).size.width),
 
-                    Padding(
-                      padding: const EdgeInsets.only(
-                          left: 25.0, right: 25.0, top: 0.0),
-                      child: mainTextWidget(context: context),
-                    ),
+                        // blessing widget
+                        BlessingWidget(),
 
-                    // Spacer between elements
-                    SizedBox(
-                        height: 15, width: MediaQuery.of(context).size.width),
-
-                    // blessing widget
-                    BlessingWidget(),
-
-                    // end spacer
-                    SizedBox(
-                        height: 50, width: MediaQuery.of(context).size.width),
-                  ],
-                )
-
-
-
-
-                ),
+                        // end spacer
+                        SizedBox(
+                            height: 50,
+                            width: MediaQuery.of(context).size.width),
+                      ],
+                    )),
               ],
             )
           ],
@@ -132,9 +134,101 @@ class ParallaxWidget extends StatelessWidget {
       left: 0,
       top: top,
       child: Container(
-        height: MediaQuery.of(context).size.height,
+        height: MediaQuery.of(context).size.height * 1.2,
         width: MediaQuery.of(context).size.width,
         child: Image.asset("$asset", fit: BoxFit.fitWidth),
+      ),
+    );
+  }
+}
+
+class ParallaxWidgetText extends StatelessWidget {
+  const ParallaxWidgetText({
+    Key? key,
+    required this.top,
+    required this.text,
+  }) : super(key: key);
+
+  final double top;
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return Positioned(
+      width: MediaQuery.of(context).size.width,
+      top: top + MediaQuery.of(context).size.height / 2.2,
+      child: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 20),
+        child: CustomRichText(
+          textAlign: TextAlign.center,
+          alignment: Alignment.center,
+          textSpan:
+          TextSpan(
+            children: <TextSpan>[
+              TextSpan(
+                text: text,
+                style: TextStyle(
+                    fontSize: 50,
+                    fontFamily: 'DavidLibre',
+                    fontWeight: FontWeight.w400,
+                    shadows: [
+                      Shadow(
+                        offset: Offset(0.0, 0.0),
+                        blurRadius: 20.0,
+                        color: Color(0xFF000000).withOpacity(0.4),
+                      ),
+                    ],
+                    color: Color(0xFF000000).withOpacity(0.6)),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class ParallaxWidgetText2 extends StatelessWidget {
+  const ParallaxWidgetText2({
+    Key? key,
+    required this.top,
+    required this.text,
+  }) : super(key: key);
+
+  final double top;
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return Positioned(
+      width: MediaQuery.of(context).size.width,
+      top: top + 50,
+      child: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 20),
+        child: CustomRichText(
+          textAlign: TextAlign.center,
+          alignment: Alignment.center,
+          textSpan: TextSpan(
+            children: <TextSpan>[
+              TextSpan(
+                text: text,
+                style: TextStyle(
+                    fontSize: 28,
+                    fontFamily: 'DavidLibre',
+                    fontWeight: FontWeight.w400,
+                    height: 1.4,
+                    shadows: [
+                      Shadow(
+                        offset: Offset(1.0, 1.0),
+                        blurRadius: 5.0,
+                        color: Color(0xff000000).withOpacity(0.1),
+                      ),
+                    ],
+                    color: Color(0xFF3F3F3F).withOpacity(0.3)),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
